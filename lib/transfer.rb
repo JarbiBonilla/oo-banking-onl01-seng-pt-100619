95ca5b255ea50383f6d1819bf99a8c3b90a18db3
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-11-08 04:29:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,6 +21,9 @@ class Transfer
         receiver.balance += amount
         self.status = "complete"
       else
+        else
+       self.status = "rejected"
+       return "Transaction rejected. Please check your account balance."
     end
    end
  
@@ -29,9 +32,6 @@ class Transfer
        sender.balance += amount
        receiver.balance -= amount
        self.status = "reversed"
-     else
-       self.status = "rejected"
-       return "Transaction rejected. Please check your account balance."
      end
    end
 
